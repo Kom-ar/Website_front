5f6f783e21c28dc91fe3b79d788c322998f59405
Karmyc <128127669+Karmyc@users.noreply.github.com>
2023-06-14 18:42:54 +1000

    Update kamazz.js

--- HTML2.0/kamazz.js
+++ HTML2.0/kamazz.js
@@ -1,6 +1,9 @@
 const truck = document.getElementById("truck");
 const cactus = document.getElementById("cactus");
 
+let score = 0
+const container = document.querySelector('h2')
+
 document.addEventListener("keydown", function (event) {
   document.addEventListener("keydown", function (event) {
     jump();
@@ -10,10 +13,16 @@ document.addEventListener("keydown", function (event) {
     if (truck.classList != "jump") {
       truck.classList.add("jump");
     }
+
     setTimeout(function () {
       truck.classList.remove("jump");
+      container.innerText = "Очки: "  + score
     }, 500);
   }
+
+  setTimeout(function(){
+    score++
+  }, 500)
 
   let crushed = setInterval(function () {
     let trucktop = parseInt(
@@ -25,6 +34,7 @@ document.addEventListener("keydown", function (event) {
 
     if (cactusleft < 50 && cactusleft > 0 && trucktop >= 140) {
       alert("GAME OVER");
+      score = 0
     }
   }, 10);
 });
